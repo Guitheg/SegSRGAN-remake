81d09d5d89e7c086f40b09f4872ae061ca78802b
Guillaume <34913527+Guitheg@users.noreply.github.com>
2021-01-14 12:16:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ from os.path import abspath, normpath, dirname, join
 
 MAIN_PATH = normpath(abspath(dirname(__file__)))
 CONFIG_INI_FILEPATH = abspath(normpath(join(MAIN_PATH, "config.ini")))
-DATA_EXAMPLE = abspath(normpath(join(MAIN_PATH, "data_example")))
