cbe883beb7ce1cf96aaa9c402f7a381adc36255b
Guillaume <34913527+Guitheg@users.noreply.github.com>
2021-03-15 16:58:19 +0100

    up

--- osirim_scripts/build_dataset.sh
+++ osirim_scripts/build_dataset.sh
@@ -18,9 +18,9 @@ container=/logiciels/containerCollections/CUDA10/tf2-NGC-20-03-py3.sif
 python=$HOME/SSG/env/bin/python
 script=$HOME/SSG/src/SegSRGAN-remake/build_dataset.py
 
-dataset_name=complete_64_dataset_bs_72
+dataset_name=complete_64_dataset_bs_64
 csv=complete.csv
 
 module purge
 module load singularity/3.0.3
-srun singularity exec ${container} ${python} ${script} -n ${dataset_name} -csv ${csv} --save_lr -bs 72
+srun singularity exec ${container} ${python} ${script} -n ${dataset_name} -csv ${csv} --save_lr -bs 64
