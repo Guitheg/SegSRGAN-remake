2bae1ef20cb1f991d7daedc2479fe0981c96fe02
Guillaume <34913527+Guitheg@users.noreply.github.com>
2021-03-15 16:38:24 +0100

    Update testing_train.sh

--- osirim_scripts/testing_train.sh
+++ osirim_scripts/testing_train.sh
@@ -18,9 +18,9 @@ container=/logiciels/containerCollections/CUDA10/tf2-NGC-20-03-py3.sif
 python=$HOME/SSG/env/bin/python
 script=$HOME/SSG/src/SegSRGAN-remake/train.py
 
-mri_test=/projets/srm4bmri/segsrgan/training_folder/batchs/complete_dataset/LR_hr1010.nii.gz
+mri_test=/projets/srm4bmri/segsrgan/training_folder/batchs/complete_64_dataset/LR_hr1010.nii.gz
 training_name=testing_train_sr
-dataset=complete_dataset
+dataset=complete_64_dataset
 
 module purge
 module load singularity/3.0.3
