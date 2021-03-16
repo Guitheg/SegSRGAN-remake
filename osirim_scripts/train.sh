d94b7b907efd79c8bf3b72d4595ea60eb03dd3cc
Guillaume <34913527+Guitheg@users.noreply.github.com>
2021-03-16 13:07:15 +0100

    Update train.sh

--- osirim_scripts/train.sh
+++ osirim_scripts/train.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-#SBATCH --job-name=TRAIN_MAE_LOSS
-#SBATCH --output=/projets/srm4bmri/outputs/TRAIN_MAE_LOSS.out
-#SBATCH --error=/projets/srm4bmri/outputs/TRAIN_MAE_LOSS.err
+#SBATCH --job-name=TRAIN_SR_GAN
+#SBATCH --output=/projets/srm4bmri/outputs/TRAIN_SR_GAN.out
+#SBATCH --error=/projets/srm4bmri/outputs/TRAIN_SR_GAN.err
 
 #SBATCH --mail-type=END   
 #SBATCH --mail-user=guigobin@gmail.com
 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 
 #SBATCH --partition=GPUNodes
 #SBATCH --gres=gpu:1
@@ -18,9 +18,9 @@ container=/logiciels/containerCollections/CUDA10/tf2-NGC-20-03-py3.sif
 python=$HOME/SSG/env/bin/python
 script=$HOME/SSG/src/SegSRGAN-remake/train.py
 
-mri_test=/projets/srm4bmri/segsrgan/training_folder/batchs/complete_dataset/LR_hr1010.nii.gz
-training_name=train_mae_loss
-dataset=complete_dataset
+mri_test=/projets/srm4bmri/segsrgan/training_folder/batchs/complete_64_dataset_bs_16/LR_hr1010.nii.gz
+training_name=train_sr_gan
+dataset=complete_64_dataset_bs_16
 
 module purge
 module load singularity/3.0.3
