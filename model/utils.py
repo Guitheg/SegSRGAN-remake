62a0e7afbe2e5d34366220acd3884e4f3fdb556b
Guillaume <34913527+Guitheg@users.noreply.github.com>
2021-03-19 00:04:45 +0100

    Update utils.py

--- model/utils.py
+++ model/utils.py
@@ -165,7 +165,10 @@ def gradient_penalty_loss(y_true, y_pred, averaged_samples, gradient_penalty_wei
     #             - averaged_samples has dimensions (batch_size, nbr_features)
     # gradients afterwards has dimension (batch_size, nbr_features), basically
     # a list of nbr_features-dimensional gradient vectors
-
+    averaged_samples.set_shape(y_pred.shape[0], 64, 64, 64)
+    print(y_true)
+    print(y_pred)
+    print(averaged_samples)
     gradients = K.gradients(y_pred, averaged_samples)[0]
     # compute the euclidean norm by squaring ...
     gradients_sqr = K.square(gradients)
